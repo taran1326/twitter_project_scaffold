5d14deb9aa665df63fa1162cd7d602b4644ead6c
Taran Mittal <taranmittal.1717@gmail.com>
2023-03-07 16:14:59 +0530

    changes in scaffold file

---
+++ Schemas/Tweets.js
@@ -0,0 +1,3 @@
+//name the schema as TweetsSchema and collection as 'tb_tweets'
+
+module.exports = mongoose.model('tb_tweets', TweetsSchema);
---
+++ Schemas/User.js
@@ -0,0 +1,3 @@
+//Name schema as userSchema and collection as tb_user
+
+module.exports = mongoose.model('tb_user', userSchema);
