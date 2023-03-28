79b50131730e9cf3b27ab9c35193cccd6762f674
Taran Mittal <taranmittal.1717@gmail.com>
2023-03-28 11:11:54 +0530

    changes

---
+++ Controllers/Tweet.js
@@ -0,0 +1,6 @@
+const createTweetRoute = '/create-tweet';
+const feedRoute = '/feed';
+const myTweetsRoute = '/my-tweets';
+
+
+//create tweets router object or tweet routes folder and use above mentioned endpoints to create various apis.
